a0b303cfc78afd893e1b68c1c3a4191178ced8a0
Seth Tierney <tierneyS@bloomington.in.gov>
2018-09-14 16:39:21 -0400

    fix pathing error with apache config

--- codeDeploy-scripts/deps-afterinstall.sh
+++ codeDeploy-scripts/deps-afterinstall.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 cd /srv/sites/touri
+a2dissite 000-default
+a2ensite touri.conf
 composer install
 node install
--- codeDeploy-scripts/deps.sh
+++ codeDeploy-scripts/deps.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
 apt-get -y install apache2 php composer nodejs
-a2dissite 000-default
-a2ensite touri.conf
