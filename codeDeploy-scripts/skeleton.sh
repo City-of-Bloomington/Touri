f42c7c5405fc1aa225e5199d7a75fc7c34c2c7ed
sethCOB <31547590+sethCOB@users.noreply.github.com>
2018-09-14 22:17:56 -0400

    create database dirs so composer doesn't fail

--- codeDeploy-scripts/skeleton.sh
+++ codeDeploy-scripts/skeleton.sh
@@ -3,7 +3,9 @@
 # Create some empty directories, otherwise composer complains on dependency install
 mkdir -p /srv/sites/touri/public
 cd /srv/sites/touri
+mkdir -p database/factories
 mkdir -p database/seeds
+mkdir -p database/migrations
 mkdir -p storage/app/public
 mkdir -p storage/framework/cache
 mkdir -p storage/framework/sessions
